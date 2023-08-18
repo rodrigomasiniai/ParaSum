74edef335632b84c85019ec1a08df26f9836b11b
Rodrigo Masini <104578949+rodrigomasiniai@users.noreply.github.com>
2023-08-18 15:08:17 -0400

    Update paraphraseApp.py

--- apps/paraphraseApp.py
+++ apps/paraphraseApp.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from paraphraser import text_paraphrase
 
-def app():
-    st.title('Paraphraser')
-    st.write('Please provide the text to be paraphrased')
-    user_input = st.text_area('Enter text','')
+
+st.title('Paraphraser')
+st.write('Please provide the text to be paraphrased')
+user_input = st.text_area('Enter text','')
+if user_input:        
     output = text_paraphrase(user_input)
     st.write("Paraphrased Text: ")
     st.write(output)
